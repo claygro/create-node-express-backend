129e37d8ff70c0a7c0837a0d496259f669c0002c
claygro <dhamala80130@gmail.com>
2026-07-29 08:29:13 +0545

    added authentication models boilerplate

--- models.js
+++ models.js
@@ -1,6 +1,30 @@
 import fs from "node:fs";
 function createModels(projectName) {
   //create auth.models.js in models
-  fs.writeFileSync(`${projectName}/src/models/auth.models.js`, "hello world");
+  fs.writeFileSync(
+    `${projectName}/src/models/auth.models.js`,
+    `
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    unique: [true, "username should be unique"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    unique: [true, "email should be unique"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+
+  },
+});
+const UserModel = mongoose.model("user", userSchema);
+export default UserModel;
+    `,
+  );
 }
 export default createModels;
